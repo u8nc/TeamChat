4b458d2d7302c2d38d5e9a1982ecb792f8ed9166
Ryan Ver Maas <ryanjvm@gmail.com>
2018-10-16 15:38:50 +1000

    Initial Commit

---
+++ TeamChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeamChat
+//
+//  Created by luobote on 16/10/18.
+//  Copyright © 2018 u8nc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
